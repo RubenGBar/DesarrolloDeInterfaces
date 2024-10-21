976848edbf5ce175e839388a2b753fd4f20201b2
RubenGBar <ruben.garcia@iesnervion.es>
2024-10-21 14:39:32 +0200

    aaaaaaaaaaa

---
+++ Unidad8/Ejercicio5Ent/clsPersona.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Ejercicio5Ent
+{
+    public class clsPersona
+    {
+        public String nombre { get; set; }
+        public String apellido { get; set; }
+        public String descripcion{ get; set; }
+    }
+}
---
+++ Unidad8/Ejercicio5UI/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Ejercicio5Ent;
+using System.Collections.ObjectModel;
+
+
+namespace Ejercicio5UI
+{
+    public partial class MainPage : ContentPage
+    {
+
+        ObservableCollection<clsPersona> personas = new ObservableCollection<clsPersona>();
+        public ObservableCollection<clsPersona> Persona { get { return personas; } }
+        public MainPage()
+        {
+            InitializeComponent();
+            personas.Add(new clsPersona() { nombre = "Pepe", descripcion="ªªªªªªªªªªªªª", apellido="Mejorada"});
+            personas.Add(new clsPersona() { nombre = "PepeLu", descripcion = "aaaaaaaaaaaaa", apellido = "Mejorado" });
+            personas.Add(new clsPersona() { nombre = "PepeLuis", descripcion = "AAAAAAAAAAAAA", apellido = "Mejoradas" });
+            personas.Add(new clsPersona() { nombre = "Jose", descripcion = "ªªªªªªªªªªªªª", apellido = "Mejorados" });
+            personas.Add(new clsPersona() { nombre = "JoseLu", descripcion = "aaaaaaaaaa", apellido = "Mejor" });
+            personas.Add(new clsPersona() { nombre = "JoseLuis", descripcion = "AAAAAAAAAAAAA", apellido = "Mejora" });
+            PersonaListView.ItemsSource = personas;
+        }
+
+    }
+
+}
