a4550b37230cefb21a5a9ef7da956e4b04ead35a
RubenGBar <ruben.garcia@iesnervion.es>
2024-10-23 14:07:09 +0200

    Ejercicio 5 terminado

--- Unidad8/Ejercicio5UI/MainPage.xaml.cs
+++ Unidad8/Ejercicio5UI/MainPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Ejercicio5Ent;
+using Ejercicio5UI.Modelos.DAL;
 using System.Collections.ObjectModel;
 
 
@@ -6,19 +7,12 @@ namespace Ejercicio5UI
 {
     public partial class MainPage : ContentPage
     {
-
-        ObservableCollection<clsPersona> personas = new ObservableCollection<clsPersona>();
-        public ObservableCollection<clsPersona> Persona { get { return personas; } }
         public MainPage()
         {
             InitializeComponent();
-            personas.Add(new clsPersona() { nombre = "Pepe", descripcion="ªªªªªªªªªªªªª", apellido="Mejorada"});
-            personas.Add(new clsPersona() { nombre = "PepeLu", descripcion = "aaaaaaaaaaaaa", apellido = "Mejorado" });
-            personas.Add(new clsPersona() { nombre = "PepeLuis", descripcion = "AAAAAAAAAAAAA", apellido = "Mejoradas" });
-            personas.Add(new clsPersona() { nombre = "Jose", descripcion = "ªªªªªªªªªªªªª", apellido = "Mejorados" });
-            personas.Add(new clsPersona() { nombre = "JoseLu", descripcion = "aaaaaaaaaa", apellido = "Mejor" });
-            personas.Add(new clsPersona() { nombre = "JoseLuis", descripcion = "AAAAAAAAAAAAA", apellido = "Mejora" });
-            PersonaListView.ItemsSource = personas;
+
+            PersonaListView.ItemsSource = lista.agregarPersonas();
+
         }
 
     }
---
+++ Unidad8/Ejercicio5UI/Modelos/DAL/lista.cs
@@ -0,0 +1,33 @@
+﻿using Ejercicio5Ent;
+using System.Collections.ObjectModel;
+
+
+namespace Ejercicio5UI.Modelos.DAL
+{
+    class lista
+    {   
+        public static ObservableCollection<clsPersona> agregarPersonas()
+        {
+            ObservableCollection<clsPersona> personas = new ObservableCollection<clsPersona> {
+
+                (new clsPersona() { nombre = "Pepe", descripcion = "ªªªªªªªªªªªªª", apellido = "Mejorada" }),
+                (new clsPersona() { nombre = "PepeLu", descripcion = "aaaaaaaaaaaaa", apellido = "Mejorado" }),
+                (new clsPersona() { nombre = "PepeLuis", descripcion = "AAAAAAAAAAAAA", apellido = "Mejoradas" }),
+                (new clsPersona() { nombre = "Jose", descripcion = "ªªªªªªªªªªªªª", apellido = "Mejorados" }),
+                (new clsPersona() { nombre = "JoseLu", descripcion = "aaaaaaaaaa", apellido = "Mejor" }),
+                (new clsPersona() { nombre = "JoseLuis", descripcion = "AAAAAAAAAAAAA", apellido = "Mejora" }),
+                (new clsPersona() { nombre = "Marta", descripcion = "AAAAAAAAAAAAA", apellido = "Requejo" }),
+                (new clsPersona() { nombre = "Yo", descripcion = "AAAAAAAAAAAAA", apellido = "García" }),
+                (new clsPersona() { nombre = "Alonso", descripcion = "AAAAAAAAAAAAA", apellido = "Fernandez" }),
+                (new clsPersona() { nombre = "Sergio", descripcion = "AAAAAAAAAAAAA", apellido = "Gonzalez" }),
+                (new clsPersona() { nombre = "Alba", descripcion = "AAAAAAAAAAAAA", apellido = "Duque" }),
+                (new clsPersona() { nombre = "Ivan", descripcion = "AAAAAAAAAAAAA", apellido = "Perez" }),
+                (new clsPersona() { nombre = "Daniel", descripcion = "AAAAAAAAAAAAA", apellido = "Rios" })
+            };
+
+            return personas;
+
+        }
+
+    }
+}
