2f5b7ca9950f60d711ca33cefb304a39605b5a65
RubenGBar <ruben.garcia@iesnervion.es>
2024-10-30 14:37:12 +0100

    Nuevo Tema

---
+++ Unidad9/EdadViewModel/ViewModels/EdadVM.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdadViewModel.ViewModels
+{
+    public class EdadVM : INotifyPropertyChanged
+    {
+        #region Atributos
+        private DateTime fechaNac;
+        private int edad;
+        #endregion
+
+        #region PropiedadesPublicas
+        public DateTime FechaNac {
+            get { return fechaNac; } 
+            set { fechaNac = value; 
+                  edad = DateTime.Now.Year - value.Year;
+                NotifyPropertyChanged("Edad");
+                }
+        }
+        public int Edad { 
+            get { return edad;  }
+        }
+        #endregion
+
+        #region Notify
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        }
+        #endregion 
+    }
+}
---
+++ Unidad9/EdadViewModel/Views/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace EdadViewModel.Views
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+ 
+    }
+
+}
